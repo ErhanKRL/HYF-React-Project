ddd32968317ede89c6b50a6bbb89d88cc109eb5b
erhan <erhan.16k@gmail.com>
2024-04-05 14:00:05 +0200

    fetching data implemented, result component modified, game logic implement

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,15 +1,20 @@
 import "../styles/Result.scss";
-export const Result = ({ isWinner, exp }) => {
-    let title;
-        if(isWinner) {
-            title = <span className="winner">WIN!</span>
-        } else {
-            title = <span className="loser">LOSE!</span>
-        }
-    return (
-        <div className="Result">
-        <h1>YOU {title}</h1>
-        <h4>Total Experience: {exp}</h4>
-        </div>
+export const Result = ({ totalExp1, totalExp2 }) => {
+  let title;
+  if (totalExp1 < totalExp2) {
+    title = (
+      <h1>
+        YOU <span className="winner">WIN!</span>
+      </h1>
     );
-}
+  } else if (totalExp1 > totalExp2) {
+    title = (
+      <h1>
+        YOU <span className="loser">LOSE!</span>
+      </h1>
+    );
+  } else {
+    title = <h1>Draw!</h1>;
+  }
+  return <div className="Result">{title}</div>;
+};
--- src/pages/Pokegame.jsx
+++ src/pages/Pokegame.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Pokeroll } from "../components/Pokeroll";
 import "../styles/Pokegame.scss";
 import { Button } from "../components/Button";
@@ -19,29 +19,80 @@ const initilalState = {
   ],
   totalExp1: 0,
   totalExp2: 0,
-  rekt: false,
   rolling: false,
-  time: 1,
+  point: 0,
 };
 export const Pokegame = () => {
   const [gameState, setGameState] = useState(initilalState);
 
+  const fetchPokemon = async (pokemonId) => {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}/`);
+    const data = await res.json();
+    const { id, name, base_experience, types } = data;
+    const type = types[0].type.name;
+    return {
+      id,
+      name,
+      type,
+      base_experience,
+    };
+  };
+
+  const createRandomIds = () => {
+    const ids = [];
+    for (let i = 0; i < 4; i++) {
+      const id = Math.floor(Math.random() * 898) + 1;
+      ids.includes(id) ? ids.push(id + 1) : ids.push(id);
+    }
+    return ids;
+  };
+
+  const createRandomHand = async () => {
+    const ids = createRandomIds();
+    const hand = await Promise.all(ids.map((id) => fetchPokemon(id)));
+    return hand;
+  };
+
+  const roll = async () => {
+    setGameState((prevState) => ({
+      ...prevState,
+      rolling: true,
+    }));
+    const hand1 = await createRandomHand();
+    const hand2 = await createRandomHand();
+    const totalExp1 = hand1.reduce(
+      (acc, pokemon) => acc + pokemon.base_experience,
+      0,
+    );
+    const totalExp2 = hand2.reduce(
+      (acc, pokemon) => acc + pokemon.base_experience,
+      0,
+    );
+    const point =
+      totalExp1 < totalExp2 ? gameState.point + 1 : gameState.point - 1;
+
+    setGameState((prevState) => ({
+      ...prevState,
+      hand1,
+      hand2,
+      totalExp1,
+      totalExp2,
+      rolling: false,
+      point,
+    }));
+  };
+
   return (
     <div className="pokegame">
-    <h2 className="Pokedex">Your Balance: {"51"}</h2>
-    {gameState.rekt === true && <h2>Your Balance is Zero</h2>}
-      <Pokeroll
-        hand={gameState.hand1}
-      />
-      <Result/>
-      <Pokeroll
-        hand={gameState.hand2}
-      />
-      <Button disabled={gameState.rolling}>
+      <h2 className="Pokedex">
+        Your Point: {gameState.point < 0 ? 0 : gameState.point}
+      </h2>
+      <Pokeroll hand={gameState.hand1} totalExp={gameState.totalExp1} />
+      <Result totalExp1={gameState.totalExp1} totalExp2={gameState.totalExp2} />
+      <Pokeroll hand={gameState.hand2} totalExp={gameState.totalExp2} />
+      <Button onClick={roll} disabled={gameState.rolling}>
         {gameState.rolling ? "Rolling!" : "Roll"}
       </Button>
-      
-      
     </div>
   );
 };
