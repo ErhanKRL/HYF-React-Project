c20b3002b53ad7dc925ae7452a4f7b473b56a1be
erhan <erhan.16k@gmail.com>
2024-04-07 17:15:09 +0200

    landing page picture changed

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -5,14 +5,14 @@ export const Landing = () => {
   return (
     <div className="landing-page">
       <div className="center-content">
-        <h1>Poke Game Center</h1>
-        <img src="https://via.placeholder.com/600x400" alt="Popular Pokemons" />
+        <h1>Poke Game</h1>
+        <img src="/pngegg.png" alt="Popular Pokemons" />
         <p>
           Welcome to the ultimate Poke Game Center where you can embark on
           exciting adventures with your favorite Pokemons!
         </p>
         <Button>
-          <Link to={"/pokegame"}>Go to App</Link>
+          <Link className="gotoApp" to={"/pokegame"}>Go to App</Link>
         </Button>
       </div>
     </div>
