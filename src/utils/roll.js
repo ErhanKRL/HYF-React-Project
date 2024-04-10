b67c011528438302076e5dc99b672fc691d05d74
erhan <erhan.16k@gmail.com>
2024-04-10 14:58:36 +0200

    fix fetching logic fault

--- src/utils/roll.js
+++ src/utils/roll.js
@@ -13,8 +13,8 @@ const createRandomHand = async (hand1, hand2) => {
   const newHand1 = [...hand1];
   const newHand2 = [...hand2];
   const ids = createRandomIds();
+  const set = await Promise.all(ids.map((id) => fetchPokemon(id)));
   for (let i = 0; i < 8; i++) {
-    const set = await Promise.all(ids.map((id) => fetchPokemon(id)));
     if (i < 4) {
       hand1[i].push(set[i]);
       hand1[i].shift();
